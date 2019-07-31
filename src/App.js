bcb5ba1a1d140f38866bd9a9290c476465e522c0
Peter Perhac <perhac.peter@gmail.com>
2019-07-31 16:09:53 +0100

    starting out

--- src/App.js
+++ src/App.js
@@ -1,26 +1,22 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import $ from 'jquery';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+
+    let url = "";
+
+    $(document).ready(function () {
+        url = new URL(window.location.href);
+    });
+
+    return (
+        <div className="App">
+            <h2>Something goes here</h2>
+            <p>{url}</p>
+        </div>
+    );
+
 }
 
 export default App;
